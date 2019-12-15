ee7b21ea62db444d164ba2f66123a19127ae0d53
Folsten <mega4642179@gmail.com>
2019-12-15 12:19:13 +0300

    first structure changes, nothing serious

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
-app.listen(3000);
+app.get('/autorization', (req, res) => {
+  res.send('Authorization page');
+})
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
